36100ba334ab2df5b4e019ec9e6e8fb5d4a13884
mssisodiya <mssisodiya@bestpeers.com>
2020-12-14 17:44:54 +0530

    used hook

--- src/counter.js
+++ src/counter.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Counter extends Component {
-  state = {
-    count: 0,
-  };
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
-        <h2>{this.state.count}</h2>
-        <button
-          className="btn btn-primary btn-md"
-          onClick={() => this.setState({ count: this.state.count + 1 })}
-        >
-          Click Me
-        </button>
-      </div>
-    );
-  }
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <button
+        className="btn btn-primary btn-md"
+        onClick={() => setCount(count + 1)}
+      >
+        Click
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
